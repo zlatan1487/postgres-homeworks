666a30a58f05a722990dedd5530b3e8ec6b7d842
kirillskyeng <kirill.netreba@skyeng.ru>
2023-02-28 16:34:27 +0300

    FIX

---
+++ homework-4/README.md
@@ -0,0 +1,10 @@
+# Data Definition Language. Домашнее задание
+
+## Написание sql-запросов
+
+- Напишите sql-запросы в файле `alter_student.sql`
+- Напишите sql-запросы в файле  `alter_northwind.sql`
+
+> **Note**
+> <br>
+> Писать и проверять запросы можно в pgAdmin, а далее код перенести в .sql-файлы.
---
+++ homework-4/alter_student.sql
@@ -0,0 +1,19 @@
+-- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
+
+
+-- 2. Добавить в таблицу student колонку middle_name varchar
+
+
+-- 3. Удалить колонку middle_name
+
+
+-- 4. Переименовать колонку birthday в birth_date
+
+
+-- 5. Изменить тип данных колонки phone на varchar(32)
+
+
+-- 6. Вставить три любых записи с автогенерацией идентификатора
+
+
+-- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
