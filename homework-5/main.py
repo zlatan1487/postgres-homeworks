f0b4103d0d90d2895b4be9626b649da88a23f414
kirillskyeng <kirill.netreba@skyeng.ru>
2023-03-23 06:20:56 +0300

    Add homework-5

---
+++ homework-5/main.py
@@ -0,0 +1,73 @@
+import json
+
+import psycopg2
+
+from config import config
+
+
+def main():
+    script_file = 'fill_db.sql'
+    json_file = 'suppliers.json'
+    db_name = 'my_new_db'
+
+    params = config()
+    conn = None
+
+    create_database(params, db_name)
+    print(f"БД {db_name} успешно создана")
+
+    params.update({'dbname': db_name})
+    try:
+        with psycopg2.connect(**params) as conn:
+            with conn.cursor() as cur:
+                execute_sql_script(cur, script_file)
+                print(f"БД {db_name} успешно заполнена")
+
+                create_suppliers_table(cur)
+                print("Таблица suppliers успешно создана")
+
+                suppliers = get_suppliers_data(json_file)
+                insert_suppliers_data(cur, suppliers)
+                print("Данные в suppliers успешно добавлены")
+
+                add_foreign_keys(cur, json_file)
+                print(f"FOREIGN KEY успешно добавлены")
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def create_database(params, db_name) -> None:
+    """Создает новую базу данных."""
+    pass
+
+def execute_sql_script(cur, script_file) -> None:
+    """Выполняет скрипт из файла для заполнения БД данными."""
+
+
+
+def create_suppliers_table(cur) -> None:
+    """Создает таблицу suppliers."""
+    pass
+
+
+def get_suppliers_data(json_file: str) -> list[dict]:
+    """Извлекает данные о поставщиках из JSON-файла и возвращает список словарей с соответствующей информацией."""
+    pass
+
+
+def insert_suppliers_data(cur, suppliers: list[dict]) -> None:
+    """Добавляет данные из suppliers в таблицу suppliers."""
+    pass
+
+
+def add_foreign_keys(cur, json_file) -> None:
+    """Добавляет foreign key со ссылкой на supplier_id в таблицу products."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
