594ee2862dc1822cdd6810c08f7e1a4758f9742c
zlatan1487 <vitalik.karpukhin@list.ru>
2023-06-13 21:53:58 +0200

    creating the three job

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,39 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
+SELECT c.company_name, CONCAT(e.first_name, ' ', e.last_name) AS employee_name
+FROM customers c, employees e, orders o, shippers s
+WHERE c.city = 'London' AND e.city = 'London'
+  AND c.customer_id = o.customer_id
+  AND e.employee_id = o.employee_id
+  AND o.ship_via = s.shipper_id
+  AND s.company_name = 'United Package';
 
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT p.product_name, p.units_in_stock, s.contact_name, s.phone
+FROM products p
+JOIN suppliers s ON p.supplier_id = s.supplier_id
+JOIN categories c ON p.category_id = c.category_id
+WHERE p.discontinued <> 1
+  AND p.units_in_stock < 25
+  AND c.category_name IN ('Dairy Products', 'Condiments')
+ORDER BY p.units_in_stock ASC;
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT company_name
+FROM customers
+WHERE customer_id NOT IN (SELECT customer_id FROM orders);
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT product_name
+FROM products
+WHERE product_id IN (
+  SELECT product_id
+  FROM order_details
+  WHERE quantity = 10
+);
