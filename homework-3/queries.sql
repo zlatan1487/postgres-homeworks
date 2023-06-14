6951271f8c7aa7c67667de51a13e55ec2c1232ed
zlatan1487 <vitalik.karpukhin@list.ru>
2023-06-14 20:02:10 +0200

    creating the three job

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,18 +2,18 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 SELECT c.company_name, CONCAT(e.first_name, ' ', e.last_name) AS employee_name
-FROM customers c, employees e, orders o, shippers s
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN employees e ON e.employee_id = o.employee_id
+JOIN shippers s ON o.ship_via = s.shipper_id
 WHERE c.city = 'London' AND e.city = 'London'
-  AND c.customer_id = o.customer_id
-  AND e.employee_id = o.employee_id
-  AND o.ship_via = s.shipper_id
   AND s.company_name = 'United Package';
-
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
+
 SELECT p.product_name, p.units_in_stock, s.contact_name, s.phone
 FROM products p
 JOIN suppliers s ON p.supplier_id = s.supplier_id
@@ -24,16 +24,18 @@ WHERE p.discontinued <> 1
 ORDER BY p.units_in_stock ASC;
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-SELECT company_name
-FROM customers
-WHERE customer_id NOT IN (SELECT customer_id FROM orders);
+SELECT c.company_name
+FROM customers c
+LEFT JOIN orders o ON c.customer_id = o.customer_id
+WHERE o.order_id IS NULL
+ORDER BY c.company_name ASC;
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
-SELECT product_name
-FROM products
-WHERE product_id IN (
-  SELECT product_id
-  FROM order_details
-  WHERE quantity = 10
-);
+SELECT DISTINCT p.product_name
+FROM Products p
+JOIN (
+    SELECT product_id
+    FROM order_details
+    WHERE quantity = 10
+) od ON p.product_id = od.product_id;
