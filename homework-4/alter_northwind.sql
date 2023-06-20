4c122951fef4505f46c0af50d127ae39d2594c59
zlatan1487 <vitalik.karpukhin@list.ru>
2023-06-20 20:17:19 +0200

    creating the fourth job

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -1,12 +1,57 @@
 -- Подключиться к БД Northwind и сделать следующие изменения:
 -- 1. Добавить ограничение на поле unit_price таблицы products (цена должна быть больше 0)
-
+ALTER TABLE products
+ADD CONSTRAINT check_unit_price_positive
+CHECK (unit_price > 0);
 
 -- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
+ALTER TABLE products
+ADD CONSTRAINT check_discontinued_value CHECK (discontinued IN (0, 1));
 
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
-
+CREATE TABLE discontinued_products AS
+SELECT *
+FROM products
+WHERE discontinued = 1;
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
+-- создание временной таблицы
+
+CREATE TABLE temp_products (
+    product_id INT PRIMARY KEY,
+    product_name character varying(40) NOT NULL,
+    supplier_id smallint,
+    category_id smallint,
+    quantity_per_unit character varying(20),
+    unit_price real,
+    units_in_stock smallint,
+    units_on_order smallint,
+    reorder_level smallint,
+    discontinued integer NOT NULL
+);
+
+-- бекап таблицы с discontinued = 0
+INSERT INTO temp_products (product_id, product_name, supplier_id,
+category_id, quantity_per_unit, unit_price, units_in_stock, units_on_order, reorder_level, discontinued)
+SELECT product_id, product_name, supplier_id,
+category_id, quantity_per_unit, unit_price, units_in_stock, units_on_order, reorder_level, discontinued
+FROM products
+WHERE discontinued = 0;
+
+-- Удаление ограничения внешнего ключа "fk_order_details_products"
+ALTER TABLE order_details DROP CONSTRAINT fk_order_details_products;
+
+-- Удаление таблицы "products"
+DROP TABLE products;
+
+-- переименование таблицы в products
+ALTER TABLE temp_products RENAME TO products;
+
+SELECT * FROM products
+
+-- тестим связь между табл., products и order_details
+SELECT *
+FROM products
+JOIN order_details ON products.product_id = order_details.product_id;
--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,36 @@
--- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
 
+-- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
+CREATE TABLE student (
+  student_id serial PRIMARY KEY,
+  first_name varchar,
+  last_name varchar,
+  birthday date,
+  phone varchar
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
+ALTER TABLE student
+ADD COLUMN middle_name varchar;
 
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE student
+DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE student
+RENAME COLUMN birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE student
+ALTER COLUMN phone TYPE varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO student (first_name, last_name, birth_date, phone)
+VALUES
+  ('luka', 'modrich', '1984-05-15', '1234567890'),
+  ('olivie', 'gerou', '1983-08-20', '9876543210'),
+  ('zlatan', 'ibrahimovich', '1983-11-10', '777755555');
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE student RESTART IDENTITY;
